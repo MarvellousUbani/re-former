1aca9f19b4d39a8ce63308a30fc29c58714c2b0e
Marvellous Michaels <marvellousubani@gmail.com>
2019-11-25 18:52:29 +0100

    fixed issues

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		  # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 		  @user = User.new(user_params)
       if @user.save
         redirect_to new_user_path
@@ -16,6 +15,16 @@ class UsersController < ApplicationController
 
 	def edit
 		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to edit_user_path
+    else
+      render 'edit'
+    end
 	end
 
 	private
